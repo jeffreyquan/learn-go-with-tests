fd017d4958bcc5cc9b89a38614b8a31ff6c5b808
Jeffrey <jeffrey.quan@hotmail.com>
2022-03-15 23:26:57 +1100

    refactor: clean test block

--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -3,15 +3,12 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
+	numbers := []int{1, 2, 3}
 
-		got := Sum(numbers)
-		want := 6
+	got := Sum(numbers)
+	want := 6
 
-		if got != want {
-			t.Errorf("got %d want %d given %v", got, want, numbers)
-		}
-	})
-
+	if got != want {
+		t.Errorf("got %d want %d given %v", got, want, numbers)
+	}
 }
